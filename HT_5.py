dd7eaf09965a30c9aca907108f011f904422cc44
DiaZa13 <cor191025@uvg.edu.gt>
2020-03-04 18:11:53 -0600

    Prueba hecha con 5 procesos

--- HT_5.py
+++ HT_5.py
@@ -1,28 +1,31 @@
+from colorama import Fore, Back, Style, init
 import simpy
 import random
 
+init(autoreset = True)
 
 def proceso(env,name,RAM,CPU,IO,arrivingTime,amount,instructions,einstructions):
+    global startTime
     #Condicionamiento de tiempo que tarda en ejecutar las instrucciones
-    if instructions < 3:
-        attentionTime = 2
-    else:
-        attentionTime = 4
     
     #Simula la llegada de los proceso
     yield env.timeout(arrivingTime)
-    print('El %s ha llegado en el tiempo %d y cuenta con %s instrucciones' % (name, env.now,instructions))
+    print('\x1b[1;37;44m'+'El %s ha llegado en el tiempo %d y cuenta con %s instrucciones' % (name, env.now,instructions))
+    
     
     #Pide una cantidad de memoria para continuar con el proceso
     yield RAM.get(amount)
     print('El %s ha tomado %d Megabytes de memoria RAM' % (name, amount))
+    startTime = env.now
     
+    #Ejecuta la funcion running
     env.process(running(env,name,CPU,instructions,einstructions,RAM))
-    
-    
-    
-                
+     
+       
 def running(env,name,CPU,instructions,einstructions,RAM):
+    
+    global totalTime
+    
     #El proceso está listo para ser ejecutado, solo falta que lo atienda el CPU
     with CPU.request() as req:  #Se pide atención por el CPU
         yield req
@@ -32,21 +35,29 @@ def running(env,name,CPU,instructions,einstructions,RAM):
             print('El CPU ha iniciado a ejecutar %s instrucciones del %s en el tiempo %s' % (instructions,name, env.now))
             yield env.timeout(2) #Tiempo que tarda en ejecutar menos de 3 instrucciones
             yield RAM.put(amount) #Como ya termino de ejecutar sus instrucciones, entonces regresa la memoria
-            print('El %s ha finalizado la ejecucicion de sus instrucciones en el tiempo %s' % (name, env.now))
+            print('\033[1;37;42m'+'El %s ha finalizado la ejecucicion de sus instrucciones en el tiempo %s' % (name, env.now))
+            endTime = env.now
+            processTime = endTime - startTime
+            print('\033[1;37;41m''El tiempo que se ha tardado en ejecutar el %s es de: %s' %(name,processTime))
+            totalTime += processTime
             instructions = 0
             
         elif (instructions == einstructions): #Cuando el proceso tiene 3 instrucciones
             print('El CPU ha iniciado a ejecutar 3 instrucciones del %s en el tiempo %s' % (name, env.now))
             yield env.timeout(3) #Tiempo que tarda en ejecutar 3 instrucciones
             yield RAM.put(amount) #Como ya termino de ejecutar las 3 instrucciones, regresa la memoria y se libera el CPU
-            print('El %s ha finalizado la ejecucicion de sus instrucciones en el tiempo %s' % (name, env.now))
+            print('\033[1;37;42m'+ 'El %s ha finalizado la ejecucicion de sus instrucciones en el tiempo %s' % (name, env.now))
+            endTime = env.now
+            processTime = endTime - startTime
+            print('\033[1;37;41m''El tiempo que se ha tardado en ejecutar el %s es de: %s' %(name,processTime))
+            totalTime += processTime
             instructions = 0
             
         else: #Cuando el proceso tiene más de 3 instrucciones
             print('El CPU ha iniciado a ejecutar %s instrucciones del %s en el tiempo %s' % (einstructions,name,env.now))
             yield env.timeout(3) #Tiempo que tarda en ejecutar 3 instrucciones
             instructions -= 3
-            print('El %s aun cuenta con %s instrucciones restantes' % (name, instructions))
+            print('\033[1;31m' + 'El %s aun cuenta con %s instrucciones restantes' % (name, instructions))
             
     if(instructions > 0):
         decision = random.randint(1,2) #Cuando sale del CPU analiza si va a ir a waiting o regresa a ready
@@ -61,21 +72,23 @@ def running(env,name,CPU,instructions,einstructions,RAM):
         else:
             print('El %s se dirige a la cola, para esperar la atencion del CPU' % (name))
             env.process(running(env,name,CPU,instructions,einstructions,RAM))
-    
-         
-    #Cuando las instruccioes son mayores que 3 lo que hace es que realiza las instrucciones que puede y lo que haya quedado como instrucciones restantes o el resto de procesos que encesita
-    #ejecutar los hacer fuera del with para que asi agarre otras instrucciones
-    
+
+
+
+
+#Datos variables a lo largo de las simulaciones
+totalTime = 0    
 env = simpy.Environment()
 SEED = 10
 RAM = simpy.Container(env,init = 100, capacity = 100)
 CPU = simpy.Resource(env, capacity = 1)
 IO = simpy.Resource(env, capacity = 1)
+NO_PROCESS = 5
 
 print('Simulacion de Ejecucion de Programas')
 random.seed(SEED)
                 
-for i in range(5):
+for i in range(NO_PROCESS):
     #timeDriving = random.randint(1,5)
     arrivingTime = random.expovariate(1.0/10)
     amount = random.randint(1,10)
@@ -84,7 +97,9 @@ for i in range(5):
     
     
 #Se ejecuta la simulacion
-env.run()    
+env.run()
+print()
+print('\033[1;30m'+'PROMEDIO:' , (totalTime/NO_PROCESS))
             
             
             
